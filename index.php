11ee944667db3e6354f2b330881361e742ff421d
Jasmine <88195169+JasmineH2212@users.noreply.github.com>
2024-07-15 14:29:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Basic PHP Page</title>
 </head>
 <body>
-    <h1>Hello, This is my first php page.</h1>
+    <h1>Hello Friends, This is my first php page.</h1>
 
     <p>Current server time: <?php echo date('Y-m-d H:i:s'); ?></p>
 
